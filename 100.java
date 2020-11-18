cc19205a888bb283419291a89cee540d0ad4f26b
Nelson-Wang <57356467+Nelson-Wang@users.noreply.github.com>
2020-11-18 11:12:01 +0800

    Create 100.java

---
+++ 100.java
@@ -0,0 +1,25 @@
+package com;
+
+/**
+ * Created by wWX945273 on 2020/11/17.
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution100 {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p==null && q==null) return true;
+        if (p==null || q==null || p.val != q.val) return false;
+        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+    }
+}
