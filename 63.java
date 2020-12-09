dbe0ba960add8058c6884ef3bd6f8a2b5a8e4610
Nelson-Wang <57356467+Nelson-Wang@users.noreply.github.com>
2020-12-09 11:05:15 +0800

    Update 63.java

--- 63.java
+++ 63.java
@@ -9,37 +9,24 @@ public class Solution63 {
         int N = obstacleGrid[0].length;
         //创建数组
         int[][] f = new int[M][N];
-        //初始条件
-        for (int k=0; k<N; k++){
-            if (obstacleGrid[0][k]!=1){
-                f[0][k] = 1;
-            }else {
-                break;
-            }
-        }
-        for (int m=0; m<M; m++){
-            if (obstacleGrid[m][0]!=1){
-                f[m][0] = 1;
-            }else {
-                break;
-            }
-        }
 
-
-        for (int i=1; i<M; i++){
-            for (int j=1; j<N; j++){
+        for (int i=0; i<M; i++){
+            for (int j=0; j<N; j++){
                 //状态方程（自己和前一步不能有障碍）
                 if (obstacleGrid[i][j]==1){
                     f[i][j] = 0;
                 }else {
-                    if (obstacleGrid[i-1][j]!=1 && obstacleGrid[i][j-1]!=1){
-                        f[i][j] = f[i-1][j] + f[i][j-1];
-                    }else if (obstacleGrid[i-1][j]==1 && obstacleGrid[i][j-1]!=1){
-                        f[i][j] = f[i][j-1];
-                    }else if (obstacleGrid[i-1][j]!=1 && obstacleGrid[i][j-1]==1){
-                        f[i][j] = f[i-1][j];
+                    //初始条件
+                    if (i==0 && j==0){
+                        f[0][0] =1;
                     }else {
                         f[i][j] = 0;
+                        if (i-1>=0){
+                            f[i][j] += f[i-1][j];
+                        }
+                        if (j-1>=0){
+                            f[i][j] += f[i][j-1];
+                        }
                     }
                 }
             }
